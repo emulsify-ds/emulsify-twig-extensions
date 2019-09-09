4568e80637e6f34cd6249b2a62948ac7902cccd8
Evan Willhite <evan@fourkitchens.com>
2019-09-09 12:58:50 -0500

    extensions code

--- packages/add-attributes-twig-extension/lib/add-attributes-twig-extension.js
+++ packages/add-attributes-twig-extension/lib/add-attributes-twig-extension.js
@@ -3,5 +3,28 @@
 module.exports = addAttributesTwigExtension;
 
 function addAttributesTwigExtension() {
-    // TODO
+  Twig.extendFunction("add_attributes", function(additional_attributes = []) {
+    attributes = [];
+  
+    for (const [key, value] of Object.entries(additional_attributes)) {
+      // If not keys array.
+      if (key !== '_keys') {
+        // If multiples items in value as array (e.g., class: ['one', 'two']).
+        if (Array.isArray(value)) {
+          attributes.push(key + '="' + value.join(' ') + '"');
+        }
+        else {
+          // Handle bem() output (pass in exactly the result).
+          if (value.includes('=')) {
+            attributes.push(value);
+          }
+          else {
+            attributes.push(key + '="' + value + '"');
+          }
+        }
+      }
+    }
+  
+    return attributes.join(' ');
+  });
 }
--- packages/bem-twig-extension/lib/bem-twig-extension.js
+++ packages/bem-twig-extension/lib/bem-twig-extension.js
@@ -3,5 +3,41 @@
 module.exports = bemTwigExtension;
 
 function bemTwigExtension() {
-    // TODO
+  Twig.extendFunction("bem", function(base_class, modifiers = [], blockname = '', extra = []) {
+    let classes = [];
+    
+    // If using a blockname to override default class.
+    if (blockname.length) {
+        // Set blockname class.
+        classes.push(blockname + '__' + base_class);
+    
+        // Set blockname--modifier classes for each modifier.
+        if (modifiers.length && Array.isArray(modifiers)) {
+        modifiers.forEach(function(modifier) {
+            classes.push(blockname + '__' + base_class + '--' + modifier);
+        });
+        }
+    }
+    // If not overriding base class.
+    else {
+        // Set base class.
+        classes.push(base_class);
+        // Set base--modifier class for each modifier.
+        if (modifiers.length && Array.isArray(modifiers)) {
+        modifiers.forEach(function(modifier) {
+            classes.push(base_class + '--' + modifier);
+        });
+        }
+    }
+    
+    // If extra non-BEM classes are added.
+    if (extra.length && Array.isArray(extra)) {
+        extra.forEach(function(extra_class) {
+        classes.push($extra_class);
+        });
+    }
+    
+    attributes = 'class="' + classes.join(' ') + '"';
+    return attributes;
+  });
 }
