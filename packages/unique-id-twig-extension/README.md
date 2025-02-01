48d9a14c6d4dc8175815e366e5ae024064d4b6eb
Marc Berger <marc@fourkitchens.com>
2025-01-31 16:40:23 -0800

    feat: Add unique ID function for JS / Storybook

--- packages/unique-id-twig-extension/README.md
+++ packages/unique-id-twig-extension/README.md
@@ -1,11 +1,17 @@
-# `add-attributes-twig-extension`
+# `unique-id-twig-extension`
 
-> TODO: description
+Generates a unique identifier and does not reuse the same identifier used elsewhere on the same page. Optionally, pass in a parameter and it will generate with a prefix.
 
 ## Usage
 
 ```
-const addAttributesTwigExtension = require('add-attributes-twig-extension');
 
-// TODO: DEMONSTRATE API
+// Generates a unique ID.
+// Outputs: qv1qqdeyms (or other random string)
+{{ uniqueId() }}
+
+// Generates a unique ID with a prefix.
+// Outputs: prefix-yq9pqk9moz (or other random string, prefixed)
+{{ uniqueId('prefix') }}
+
 ```
--- packages/unique-id-twig-extension/lib/unique-id-twig-extension.js
+++ packages/unique-id-twig-extension/lib/unique-id-twig-extension.js
@@ -3,35 +3,56 @@
 module.exports = uniqueIdTwigExtension;
 
 function uniqueIdTwigExtension(Twig) {
-  Twig.extendFunction("uniqueId", function(additional_attributes = [], attributes = '') {
-  console.log('Inside unique ID function 789');
-    //   attributes = [];
+  Twig.extendFunction("uniqueId", function(prepend = '') {
 
-  //   for (const [key, value] of Object.entries(additional_attributes)) {
-  //     // If not keys array.
-  //     if (key !== '_keys') {
-  //       switch (typeof value) {
-  //         case 'string':
-  //         case 'boolean':
-  //         case 'number':
-  //           // Handle bem() output (pass in exactly the result).
-  //           if (typeof value === 'string' && value.includes('=')) {
-  //             attributes.push(String(value));
-  //           }
-  //           else {
-  //             attributes.push(key + '="' + String(value) + '"');
-  //           }
-  //           break;
-  //         case 'object':
-  //           // use Array.isArray to differentiate regular objects from arrays
-  //           if (Array.isArray(value)) {
-  //             attributes.push(key + '="' + value.join(' ') + '"');
-  //           }
-  //           break;
-  //       }
-  //     }
-  //   }
+    class UniqueIdGenerator {
+      constructor() {
+        // Storage for unique ID on same page.
+        this.uniqueIds = new Set();
+      }
 
-  //   return attributes.join(' ');
+      /**
+       * Generate a unique ID. Ensures unique on same page.
+       *
+       * @param {string} [prepend] - An optional string to prepend to the ID.
+       * @return {string} - A unique ID, optionally prepended.
+       */
+      getUniqueId(prepend = '') {
+        let id = this.generateRandomId();
+
+        // Check if ID has already been used on the page.
+        while (this.uniqueIds.has(id)) {
+          id = this.generateRandomId();
+        }
+
+        this.uniqueIds.add(id);
+
+        return prepend ? `${prepend}-${id}` : id;
+      }
+
+      /**
+       * Generate a random ID.
+       *
+       * @param {number} length - The length of the generated ID (default 10).
+       * @return {string} - A randomly generated string.
+       */
+      generateRandomId(length = 10) {
+        const letters = 'abcdefghijklmnopqrstuvwxyz';
+        const chars = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+        // First letter must be a letter for DOM ids.
+        let id = letters[Math.floor(Math.random() * letters.length)];
+
+        for (let i = 1; i < length; i++) {
+          id += chars[Math.floor(Math.random() * chars.length)];
+        }
+
+        return id;
+      }
+    }
+
+    const idGenerator = new UniqueIdGenerator();
+    return idGenerator.getUniqueId(prepend);
+
   });
 }
